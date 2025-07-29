d402be14c9233e3c994600d40bbfcc80c961495d
James <jamesst@seycove.ca>
2025-07-29 00:42:44 -0700

    Added Drivers and Micropython

--- Firmware/logic.py
+++ Firmware/logic.py
@@ -5,12 +5,12 @@ from machine import Pin, I2C, PWM
 import pn532
 
 # --- Wi-Fi Setup ---
-SSID = 'YourWiFiSSID'
-PASSWORD = 'YourWiFiPassword'
+SSID = 'YourWiFiSSID' #add the stuff
+PASSWORD = 'YourWiFiPassword' #and this stuff
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-wlan.connect(SSID, PASSWORD)
+wlan.connect(SSID, PASSWORD) #and even more stuff
 
 print("Connecting to Wi-Fi...")
 while not wlan.isconnected():
@@ -19,7 +19,7 @@ print("Connected, IP:", wlan.ifconfig()[0])
 
 # --- GPIO Setup ---
 
-# I2C for PN532 NFC
+# I2C for NFC
 i2c = I2C(1, scl=Pin(23), sda=Pin(22), freq=400000)
 pn532_module = pn532.PN532_I2C(i2c, debug=False)
 
@@ -28,23 +28,23 @@ print('Found PN532 with firmware version: {0}.{1}'.format(ver, rev))
 
 pn532_module.SAM_configuration()
 
-# Buttons with pull-ups
+# Buttons 
 button1 = Pin(17, Pin.IN, Pin.PULL_UP)
 button2 = Pin(19, Pin.IN, Pin.PULL_UP)
 
-# LEDs as outputs
+# LEDs 
 led1 = Pin(2, Pin.OUT)
 led2 = Pin(21, Pin.OUT)
 
-# Servos on GPIO0 and GPIO1 (PWM at 50Hz)
+# Servos on GPIO0 and GPIO1 
 servo1 = PWM(Pin(0), freq=50)
 servo2 = PWM(Pin(1), freq=50)
 
-# Speaker on GPIO16 (PWM)
+# Speaker on GPIO16 
 speaker = PWM(Pin(16))
 
 # PC trigger URL
-PC_IP = '192.168.x.x'  # Replace with your PC's IP
+PC_IP = '192.168.x.x'  # Replace with IP
 URL = f'http://{PC_IP}:5000/trigger'
 
 def send_trigger():
@@ -55,31 +55,27 @@ def send_trigger():
     except Exception as e:
         print("Failed to send trigger:", e)
 
-# Button handlers
-def button1_handler(pin):
+# Button 
+def button1(pin):
     print("Button 1 pressed!")
-    led1.value(not led1.value())  # toggle LED1
-    send_trigger()
 
-def button2_handler(pin):
+def button2(pin):
     print("Button 2 pressed!")
-    led2.value(not led2.value())  # toggle LED2
-    send_trigger()
 
-button1.irq(trigger=Pin.IRQ_FALLING, handler=button1_handler)
-button2.irq(trigger=Pin.IRQ_FALLING, handler=button2_handler)
+button1.irq(trigger=Pin.IRQ_FALLING, handler=button1)
+button2.irq(trigger=Pin.IRQ_FALLING, handler=button2)
 
 # Servo control function
 def set_servo_angle(servo, angle):
-    min_duty = 26   # 0.5ms pulse width
-    max_duty = 128  # 2.5ms pulse width
+    min_duty = 26   # 0.5ms 
+    max_duty = 128  # 2.5ms 
     duty = int(min_duty + (max_duty - min_duty) * (angle / 180))
     servo.duty(duty)
 
 # Speaker beep
 def beep(frequency=1000, duration=200):
     speaker.freq(frequency)
-    speaker.duty(512)  # 50% duty cycle
+    speaker.duty(512)  
     time.sleep_ms(duration)
     speaker.duty(0)
 
