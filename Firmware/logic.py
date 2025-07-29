1d0bbaea260997ed0b2d6f50f58e1fc9407e9254
James <jamesst@seycove.ca>
2025-07-29 00:57:20 -0700

    Code Finished?

--- Firmware/logic.py
+++ Firmware/logic.py
@@ -4,91 +4,135 @@ import time
 from machine import Pin, I2C, PWM
 import pn532
 
-# --- Wi-Fi Setup ---
-SSID = 'YourWiFiSSID' #add the stuff
-PASSWORD = 'YourWiFiPassword' #and this stuff
+SSID = 'WiFiSSID'
+PASSWORD = 'WiFiPassword'
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-wlan.connect(SSID, PASSWORD) #and even more stuff
+wlan.connect(SSID, PASSWORD)
 
-print("Connecting to Wi-Fi...")
 while not wlan.isconnected():
     time.sleep(1)
 print("Connected, IP:", wlan.ifconfig()[0])
 
-# --- GPIO Setup ---
-
-# I2C for NFC
 i2c = I2C(1, scl=Pin(23), sda=Pin(22), freq=400000)
 pn532_module = pn532.PN532_I2C(i2c, debug=False)
 
 ic, ver, rev, support = pn532_module.get_firmware_version()
-print('Found PN532 with firmware version: {0}.{1}'.format(ver, rev))
-
+print('PN532 firmware: {0}.{1}'.format(ver, rev))
 pn532_module.SAM_configuration()
 
-# Buttons 
-button1 = Pin(17, Pin.IN, Pin.PULL_UP)
-button2 = Pin(19, Pin.IN, Pin.PULL_UP)
+btn1 = Pin(17, Pin.IN, Pin.PULL_UP)
+btn2 = Pin(19, Pin.IN, Pin.PULL_UP)
 
-# LEDs 
 led1 = Pin(2, Pin.OUT)
 led2 = Pin(21, Pin.OUT)
 
-# Servos on GPIO0 and GPIO1 
 servo1 = PWM(Pin(0), freq=50)
 servo2 = PWM(Pin(1), freq=50)
 
-# Speaker on GPIO16 
 speaker = PWM(Pin(16))
+speaker.duty(0)
 
-# PC trigger URL
-PC_IP = '192.168.x.x'  # Replace with IP
+PC_IP = '192.168.x.x'
 URL = f'http://{PC_IP}:5000/trigger'
 
 def send_trigger():
     try:
-        response = urequests.post(URL)
-        print("Trigger sent, response:", response.text)
-        response.close()
+        r = urequests.post(URL)
+        print("Trigger sent:", r.text)
+        r.close()
     except Exception as e:
-        print("Failed to send trigger:", e)
+        print("Trigger failed:", e)
 
-# Button 
-def button1(pin):
-    print("Button 1 pressed!")
-
-def button2(pin):
-    print("Button 2 pressed!")
-
-button1.irq(trigger=Pin.IRQ_FALLING, handler=button1)
-button2.irq(trigger=Pin.IRQ_FALLING, handler=button2)
-
-# Servo control function
-def set_servo_angle(servo, angle):
-    min_duty = 26   # 0.5ms 
-    max_duty = 128  # 2.5ms 
-    duty = int(min_duty + (max_duty - min_duty) * (angle / 180))
+def set_servo_speed(servo, speed):
+    neutral = 77
+    max_delta = 51
+    duty = int(neutral + (speed / 100) * max_delta)
     servo.duty(duty)
 
-# Speaker beep
-def beep(frequency=1000, duration=200):
-    speaker.freq(frequency)
-    speaker.duty(512)  
-    time.sleep_ms(duration)
+def beep(freq=1000, dur=200):
+    speaker.freq(freq)
+    speaker.duty(512)
+    time.sleep_ms(dur)
     speaker.duty(0)
 
-# Main loop: check for NFC tags and handle
+def siren_step(step, max_step=50, base_freq=500, freq_range=1500):
+    if step < max_step // 2:
+        freq = base_freq + (freq_range * step * 2) / max_step
+    else:
+        freq = base_freq + (freq_range * (max_step - step) * 2) / max_step
+    return int(freq)
+
 last_uid = None
+nfc_cooldown_end = 0
+
+btn2_hold_start = None
+btn2_reverse_pending = False
+siren_pos = 0
+siren_direction = 1
+max_siren_steps = 50
+
+set_servo_speed(servo1, 0)
+
 while True:
-    uid = pn532_module.read_passive_target(timeout=0.5)
-    if uid:
-        if uid != last_uid:
-            print('NFC tag detected:', [hex(i) for i in uid])
+    now = time.ticks_ms()
+
+    if time.ticks_ms() > nfc_cooldown_end:
+        uid = pn532_module.read_passive_target(timeout=0.5)
+        if uid and uid != last_uid:
+            print('NFC tag:', [hex(i) for i in uid])
             beep()
             send_trigger()
             last_uid = uid
+            nfc_cooldown_end = now + 5000
+        elif not uid:
+            last_uid = None
+
+    if not btn1.value():
+        print("Button 1 pressed")
+        beep(1200, 100)
+        set_servo_speed(servo1, 100)
+        time.sleep(1)
+        set_servo_speed(servo1, 0)
+        time.sleep(0.2)
+        set_servo_speed(servo1, -100)
+        time.sleep(1)
+        set_servo_speed(servo1, 0)
+        while not btn1.value():
+            time.sleep_ms(10)
+
+    if not btn2.value():
+        if btn2_hold_start is None:
+            btn2_hold_start = now
+            siren_pos = 0
+            siren_direction = 1
+
+        freq = siren_step(siren_pos, max_siren_steps)
+        speaker.freq(freq)
+        speaker.duty(300)
+
+        siren_pos += siren_direction
+        if siren_pos >= max_siren_steps:
+            siren_direction = -1
+            siren_pos = max_siren_steps
+        elif siren_pos <= 0:
+            siren_direction = 1
+            siren_pos = 0
+
+        if not btn2_reverse_pending and time.ticks_diff(now, btn2_hold_start) > 5000:
+            print("Button 2 held 5s, reversing servo")
+            btn2_reverse_pending = True
     else:
-        last_uid = None
-    time.sleep(0.1)
+        speaker.duty(0)
+        btn2_hold_start = None
+        siren_pos = 0
+        siren_direction = 1
+
+    if btn2_reverse_pending:
+        set_servo_speed(servo1, -100)
+        time.sleep(1)
+        set_servo_speed(servo1, 0)
+        btn2_reverse_pending = False
+
+    time.sleep_ms(10)
